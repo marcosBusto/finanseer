bcdfa11c76d90189b36dc57d79f4596893b7ca74
Marcos Busto <bustosuarezmarcos@gmail.com>
2024-06-24 16:38:39 +0200

    Dashboard Layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,11 +19,7 @@ function App() {
               <Routes>
                 <Route 
                   path="/" 
-                  element={
-                    <div>
-                      dashboard page
-                    </div>
-                  } 
+                  element={ <Dashboard />} 
                 />
                 <Route 
                   path="/predictions" 
