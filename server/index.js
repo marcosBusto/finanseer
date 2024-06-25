0bc2286b4fdf6dad9a3a3cabd3d675b4bd27c033
Marcos Busto <bustosuarezmarcos@gmail.com>
2024-06-25 17:16:16 +0200

    Products UI and API

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import Product from "./models/Product.js";
+import { kpis, products } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -24,6 +26,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -37,4 +40,5 @@ mongoose.connect(process.env.MONGO_URL, {
     /* ADD DATA ONE TIME ONLY */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
+    // Product.insertMany(products);
 }).catch((error) => console.log(`${error} did not connect`));
